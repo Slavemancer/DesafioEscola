4795608849331d809dbf5eb7163b524bc7b0ca56
Slavemancer <danielcarvalhomon@gmail.com>
2018-11-14 23:24:41 +0000

    Secon commit

--- Desafio/src/main/Main.java
+++ Desafio/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.Method;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,8 +13,7 @@ public class Main  {
 	
 	public static void main(String[] args) {
 		String[] opcoes = {"Pedra","Papel","Tesoura","Spock","Lagarto"};
-		List lista=new List();
-		Scanner Teclado= new Scanner(System.in);
+		String[] verificacao= new String;
 		Random rnd = new Random();
 		int jogadaJogador=0;
 		int jogadaComputadorInt = (int)rnd.nextInt(5);
@@ -21,16 +21,33 @@ public class Main  {
 		for(int i=0; i<=(opcoes.length)*2-1;i+=1) {
 			//System.out.println(i);
 			if (i<=4) {
-				lista.add(opcoes[i]);
+				verificacao[i]=opcoes[i];
 			}else {
-				lista.add(opcoes[(i-5)]);				
+				verificacao[i]=(opcoes[(i-5)]);				
 			}
 		}
+		do {
+			for(int i=0; i<=(opcoes.length)-1;i+=1) {
+				System.out.println(i + " : "+opcoes[i]);
+			}
+			System.out.println();
+			System.out.println("Introduza o numero correspondente á opção que pretende jogar");
+			Scanner Teclado= new Scanner(System.in);
+			jogadaJogador = Teclado.nextInt();
+			System.out.println();
+			if(jogadaJogador<0 ||  jogadaJogador>4) {
+				System.out.println("Introduziu um valor inválido");
+			}
+		}while(jogadaJogador<0 ||  jogadaJogador>4);
 		
-		for(int i=0; i<=(opcoes.length)*2-1;i+=1) {
-			System.out.println(i + " : "+opcoes[i]);
+		if(jogadaJogador==jogadaComputadorInt) {
+			System.out.println("Empate , ambos jogaram "+opcoes[jogadaComputadorInt]);
+		}else if(jogadaComputadorInt==jogadaJogador+1 || jogadaComputadorInt==jogadaJogador+3) {
+			System.out.println("Ganhaste , "+opcoes[jogadaJogador]+" vence "+opcoes[jogadaComputadorInt]);
+		}else {
+			System.out.println("Perdeste , "+opcoes[jogadaJogador]+" vence "+opcoes[jogadaJogador]);
 		}
-		jogadaJogador = Teclado.nextInt();
+		
 		System.out.println(jogadaJogador);
 	}
 
