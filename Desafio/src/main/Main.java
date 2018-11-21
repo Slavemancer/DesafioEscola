5aed00c6c6b1fd1232996e555f9a6bf76f74890c
Slavemancer <35873607+Slavemancer@users.noreply.github.com>
2018-11-21 11:55:27 +0000

    jogo 1.0 - release
    Já dá para jogar com o computador

--- Desafio/src/main/Main.java
+++ Desafio/src/main/Main.java
@@ -1,22 +1,17 @@
 package main;
 
-import java.awt.List;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.lang.reflect.Method;
 import java.util.Random;
 import java.util.Scanner;
-
-import javax.swing.JComboBox;
 
 public class Main  {
 	
 	public static void main(String[] args) {
-		String[] opcoes = {"Pedra","Papel","Tesoura","Spock","Lagarto"};
-		String[] verificacao= new String;
+		String[] opcoes = {"Tesoura","Papel","Pedra","Lagarto","Spock"};
+		String[] verificacao= new String[opcoes.length*2];
 		Random rnd = new Random();
 		int jogadaJogador=0;
 		int jogadaComputadorInt = (int)rnd.nextInt(5);
+		Scanner Teclado= new Scanner(System.in);
 		
 		for(int i=0; i<=(opcoes.length)*2-1;i+=1) {
 			//System.out.println(i);
@@ -32,23 +27,23 @@ public class Main  {
 			}
 			System.out.println();
 			System.out.println("Introduza o numero correspondente á opção que pretende jogar");
-			Scanner Teclado= new Scanner(System.in);
 			jogadaJogador = Teclado.nextInt();
 			System.out.println();
 			if(jogadaJogador<0 ||  jogadaJogador>4) {
-				System.out.println("Introduziu um valor inválido");
+				System.out.println("Introduziu um valor inv�lido");
 			}
 		}while(jogadaJogador<0 ||  jogadaJogador>4);
+		
+		Teclado.close();
 		
 		if(jogadaJogador==jogadaComputadorInt) {
 			System.out.println("Empate , ambos jogaram "+opcoes[jogadaComputadorInt]);
 		}else if(jogadaComputadorInt==jogadaJogador+1 || jogadaComputadorInt==jogadaJogador+3) {
 			System.out.println("Ganhaste , "+opcoes[jogadaJogador]+" vence "+opcoes[jogadaComputadorInt]);
 		}else {
-			System.out.println("Perdeste , "+opcoes[jogadaJogador]+" vence "+opcoes[jogadaJogador]);
+			System.out.println("Perdeste , "+opcoes[jogadaComputadorInt]+" vence "+opcoes[jogadaJogador]);
 		}
-		
-		System.out.println(jogadaJogador);
+		System.out.println();
 	}
 
 }
